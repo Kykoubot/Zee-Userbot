c15094b9efeccd7e784dc97a032716b2198405c2
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-09 21:57:31 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -48,8 +48,8 @@ async def gcast(event):
     elif event.is_reply:
         msg = await event.get_reply_message()
     else:
-        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
-    kk = await edit_or_reply(event, "`Globally Broadcasting Msg...`")
+        return await edit_delete(event, "**❆ Berikan Sebuah Pesan ❆**")
+    kk = await edit_or_reply(event, "`❆ ᴏᴛᴡ ᴍᴇɴɢɪʀɪᴍ ᴘᴇꜱᴀɴ ᴋᴇ ꜱᴇᴍᴜᴀ ɢʀᴜᴘ...`")
     er = 0
     done = 0
     async for x in event.client.iter_dialogs():
@@ -67,7 +67,7 @@ async def gcast(event):
                 except BaseException:
                     er += 1
     await kk.edit(
-        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
+        f"❆ ʙᴇʀʜᴀꜱɪʟ ᴍᴇɴɢɪʀɪᴍ ᴘᴇꜱᴀɴ ᴋᴇ `{done}` ❆ ɢᴀɢᴀʟ ᴍᴇɴɢɪʀɪᴍ ᴘᴇꜱᴀɴ ᴋᴇ `{er}` **Grup**"
     )
 
 
