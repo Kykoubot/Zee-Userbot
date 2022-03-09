995b297cacda20647fea2deef24335b079cd620f
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-10 04:30:28 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -387,7 +387,7 @@ async def check_botlog_chatid() -> None:
 async def update_restart_msg(chat_id, msg_id):
     DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
     message = (
-        f"**Zee-UserBot v{BOT_VER} is back up and running!**\n\n"
+        f"**Zee-UserBot v{BOT_VER} Sedang Berjalan!**\n\n"
         f"**Telethon:** {version.__version__}\n"
         f"**Python:** {python_version()}\n"
         f"**User:** {DEFAULTUSER}"
