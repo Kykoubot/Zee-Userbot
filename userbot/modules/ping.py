a60cb2a5b1321d6672aecb19e1b395119f0e0b10
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-10 02:35:59 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -65,8 +65,8 @@ async def _(ping):
     duration = (end - start).microseconds / 1000
     user = await ping.client.get_me()
     await xx.edit(
+        f"**PONG🏓..**\n"
         f"• **Zee-Userbot**\n"
-        f"**━━━━━━━━━━━**\n"
         f"• **Pinger** · `%sms`\n"
         f"• **Uptime ·** `{uptime}` \n"
         f"• **Master ·** [{user.first_name}](tg://user?id={user.id})" % (duration)
