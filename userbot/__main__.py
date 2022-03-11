0669128b02547010f302e53052973f57e368256a
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-12 05:19:05 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -35,7 +35,7 @@ try:
     LOGS.info(f"Python Version - {python_version()}")
     LOGS.info(f"Telethon Version - {version.__version__}")
     LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-    LOGS.info(f"Zee-Userbot Version - {ubotversion} [✨ BERHASIL DIAKTIFKAN! ✨]")
+    LOGS.info(f"Zee-Userbot Version - {ubotversion} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
 except BaseException as e:
