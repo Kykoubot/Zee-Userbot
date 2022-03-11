092894fc89fbfd54d290f8843ba7375499f174de
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-11 17:21:49 +0800

    Update botcontrol.py

--- userbot/modules/botcontrol.py
+++ userbot/modules/botcontrol.py
@@ -900,7 +900,7 @@ async def bot_start(event):
                         \n\n**Saya adalah {my_first}** \
                         \n**Anda dapat menghubungi [{OWNER}](tg://user?id={OWNER_ID}) dari sini.**\
                         \n**Jangan melakukan spam atau anda akan di Banned**\
-                        \n\n**Powered by** [UserBot](https://github.com/mrismanaziz/Man-Userbot)"
+                        \n\n**Powered by** [UserBot](https://github.com/Kykoubot/Zee-Userbot)"
         buttons = [
             (
                 Button.url("ɢʀᴏᴜᴘ", f"https://t.me/{GROUP}"),
