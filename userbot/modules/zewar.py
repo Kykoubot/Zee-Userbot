f91576a1c91cf129135440c25d191a5e17fdd56f
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-28 20:21:44 +0800

    Added war1 module to Zee-Userbot

---
+++ userbot/modules/zewar.py
@@ -0,0 +1,246 @@
+# ReCode by @zeafeya
+# FROM Zee-Userbot <https://github.com/Kykoubot/Zee-Userbot>
+# Group Support t.me/Dbzea & Channel Support t.me/Storezeastore
+# KALO COPAS INI GAUSAH DI HAPUS KONTOLLLLLLL
+
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, man_cmd
+
+
+@man_cmd(pattern="b(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "JALAN JALAN SAMA AYU TINGTING ISTRI NARUTO NAMANYA HINATA, EH DASAR KADAL BUNTING GAUSAH BELAGU DISINI LO GW SAPURATA.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="c(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "EH BOCAH TOLOL YANG KERJAAN NYA JADI GIGOLO LEBIH BAIK LU APUS TELE DARI PADA DI TELEGRAM JADI RAJA JAMET MENDING GUE YE KAN RAJA PARA ORANG CAKEP SEINDONESIA.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="d(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "JADI MANUSIA JANGAN KEBANYAKAN HALU, SADAR DIRI AJA LO ITU CUMA BENALU BIJI MATA LO CUMA SATU MUKA LO UDAH KAYA PANTAT LEMBU YANG SABAN HARI MAKAN PAKE BATU KOSAKATA LU BEGITU MULU DASAR BOCAH-BOCAH GAGU.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="e(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "DASAR MANTAN NAPI YANG KETANGKEP GARA-GARA MALING TV, LO DISINI CUMA GUA CACI MAKI KAYA ORANG GA PUNYA HARGA DIRI LEHER BEDAKI GA PERNAH MANDI, BADAN LU AJA BAU TAI, MULUT LU BAU GA PERNAH SIKAT GIGI, GIGI TONGGOS KAYA ELI SUGIGI.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="f(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "BELI PEPAYA BELI TAUCO, BELI NYA DI KERAMAT JATI, EH TOLOL GAUSAH BANYAK GAYA, LO ITU CUMA KROCO YANG SEKALI INJEK LANGSUNG MATI.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="g(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "ANAK GOBLOK ANAK ANJING ANAK KAMPANG GAUSAH BERGAYA SEPERTI SUPERSTAR KALO NYATANYA LO ITU CUMA RONTOKAN NASTAR, NYALI CUMA SEGEDE KUTIL BANCI LAGU-LAGUAN MAU ADU ROASTING AMA GUA, AELAH GADA GETERNYA GW LAWAN ANAK STRES KEK LU MAH GW JABANIN SAMPE LEHER LU MELINTIR, LU NGOMONG AJA KAYA BAYI BARU LAHIR DASAR MANUSIA AMATIR.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="h(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "MODELAN ORANG KAYA LO GA PANTES LAWAN GUA, MENDING LO SURREND DARI SEKARANG DARIPADA LO GUA BIKIN MALU. LU DISINI CUMA GUA JADIIN BAHAN HINAAN, GUA TAU LO ITU BUKAN MANUSIA MELAINKAN HEWAN DASAR SAMPAH BUANGAN.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="i(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "SADAR DIRI BOCAH LAKNAT YANG HOBI NYA CARI JIMAT CARI CARI BENDA KERAMAT EH GOBLOK GAUSAH KEBANYAKAN MAKSIAT BENTAR LAGI KIAMAT. MAKANYA JANGAN KESERINGAN NGEWE SAMA KAMBING DI BEDAKIN. GINI NIH CIRI CIRI ANAK YANG GOBLOKNYA DI FORMALIN. GAUSAH BANYAK GAYA DARI PADA NANTI LU GUA GAMPARIN DASAR PENJUAL SEMPOL BOCAH BOCAH KONTOL MUKA LO GUA TAMPOL UDAH GEDE MASIH SERING NGOMPOL.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="m(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "EH GOBLOK BAPALO TUH PENGEMIS TAPI SUKA BGT MABOK MABOKAN ANJING UDAH MISKIN HIDUPNYA JADI PENGEMIS SENGSARA MULU EH MALAH MASIH DOYAN JUDI SAMA MABOK AJA ANJING PANTES AJA HIDULO GAPERNAH BERKAH GOBLOK ORG LO DINAFKAHIN DUIT HASIL JUDI SAMA HASIL NGEMIS PAS BGT ITU DOSANYA SEIMBANG ANJING, HARAM DAN HALAL.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="n(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "MAKANYA KALOO BELUM BISA APA APA LEBIH BAIK PERGI SAJA DASAR BEGOO ANAK BAJINGAN YANG HARAM SEPERTI BABI SERING ALIBI MENTAL KECIL KYA KURCACI MINTA DI CACI MAKI",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="o(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "ETDAHHHH LU KACUNG MUKA LU BAYAK BELATUNG BENTUKAN LU AJA KYA LUTUNG MENDINGAN LU BALIK KAMPUNG KAGA USH DISINI LU GK BISA ROSTING LIDAH LU AJA KEGULUNG GULUNG NTAR LU MATI KENA FLUBURUNG",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="r(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "GUA DATANG INGIN MENGGUNCANG DENGAN SUARA GUA BIKIN MENTAL GOYANG JADI BOCAH LU JANGAN BANYAK BINCANG BANYAK BACOT KRONGKONGAN LU GUA TERJANG.",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="s(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "GUA PINTAR MAIN KATA KOSAKATA GUA PUNYA MAKNA BUKAN KAYA LU YG NGAKUNYA SEORANG RAJA TAPI KOSAKATA MUNGUT DIMANA MANA AYO LAWAN GUA SANG MENTAL BAJA KOSAKATA PANAS API NERAKA LEBIH BAHAYA DARI PETAKASIAP SIAP GUA SAPU RATA",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="t(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "ADA BOCAH BALAP KARUNG TAPI DIA MAKE SARUNG GUA KIRA INI PETARUNG EH TAUNYA CUMAN KACUNG MANUSIA TANPA HIDUNG MANUSIA GA TAU UNTUNG TITTLE CUMAN JADI TEMPAT BERLINDUNG BOCAH BARU YA KETAUAN BET KALO SITU KAMPUNG ORANG KAYA LU 10 JUGA GUA TAMPUNG BOCAH BOCAH KAYA LU COCOK NYA DI PASUNG BCANDAAN DIKIT UDAH TERSINGGUNG SINI LU NAIK GUA KASIH PANGGUNGGADA GENTARNYA GUA LADENIN BOCAH BOCAH SAGAPUNG",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="u(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "LEGENDA WAR BALIK BERKOAR NGEJABANIN LU GUA GAPERNAH GENTAR GUA KASIH KATA KATA PENAMPAR SIAP SIAP MALU TERKAPAR KAYA ORANG MABUK SAMPE TEPAR BOCAH KAYA LU KURANG BELAJARKASIH KONSEKUENSI SINI LU GUA PAPAR",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="v(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "SERAMPANG SERIMPING MUKA LU GUA BANTING MUKA JELEK KAYA KEPITING MULUT KAYA BOOL KAMBING KALO GUA LIAT LU KAYA KADAL BUNTING BOCAH BOCAH YG GABISA KENCING YG SANGE LIAT TETE ANJING BOCAH YG KERJAAN NYA NUNGGING DENGAN JEMBUT KERITING",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="w(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "MAKAN MASIH DARI TONG SAMPAH SOK HIDUP BERGAYA MEWAH MAU NGELUARIN SUMPAH SERAPAHTAPI YG KELUAR DESAHAN LACUR AH AH AH",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="x(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "YAELAH MUKA JELEK JERAWATAN PANOAN GAK ADA MALU NYA DONGO MALU MUKA LUH TUH KAYA KONTOL GUA JUGA TAHU TOLOL BET LUH IYA MAKANYA KLO PUNYA MUKA TUH GANTENG DIKIT BEGO BIAR LUH KLO KETEMU CWE GAK DI CUDAIN TOLOL KARENA GUA KASIAN GUA LIHAT MUKA LUH YANG SEKALINYA DI SUKAIN CWE TAPI CWENYA MAU SUKAIN LUH KARENA DIA MAU CUDAIN MUKA LUH BIAR LUH SADAR DIKI MUKA LUH GAK PANTES SAMA CWE YANG CANTIK TOLOL BET LUH MAKANYA TOLOL KLO PUNYA EMAK SAMA BAPAK YANG GANTENG BIAR ANAK NYA GANTENG INI MAH MAK JELEK BAPAK BUDUK BUNTUNG LAGI KAKI NYA MAKANYA BILANGIN TUH MAK LUH KLO NGEWE TUH JANGAN DI GOT KAYA TIKUS GOT  MAKANNYA MUKA ANAK NYA KAYA PANCI GOSONG KASIAN UDA MUKA JELEK CACAT LAGI TOLOL MENDING LUH BALIK TERUS BUNUH DIRI KARENA LUH GAK GUNA KLO HIDUP TAPI CACAT",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+@man_cmd(pattern="z(?: |$)(.*)")
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "WOI KUDANIL MUKA UDAH BULET KEK CAKIL BOCAH SAGAPUNG GILA PENTIL SINI DONG ITIL REP IN GUA BIAR GA KEK ORANG TOLOL GITU TYPING TIPIS BAT KEK BADAN LU UDAH MANA SISA HIDUPLU SEDIKIT BAT ANAK YANG HARAM KEK LU LAHIR DARI MAK YANG JADI KACUNG DI LUAR NEGRI MANA UDAH JADI JONGOS DICAMBUKIN KARENA GAPERNAH BENER KERJA NYA PEN BAT YA GAJI GEDE AMPE MAU DIEWE MAJIKAN AMPE HAMIL ABIS ITU DIPECAT LAGI TOLOL MENDING LU URUS BAPAKLU YANG KENA STROKE JANTUNGAN JADI PENYAKIT KOMPLIKASI BEGITU GARA GARA NGELIAT MAK LU YANG BEJAT BAT KELAKUAN NYA NOH BAPAKLU UDAH BAU BAT BAU KASURNYA BOKER KENCING SEMBARANGAN BAU BAU IN TETANGGA TOLOL MANA DIUSIR DARI DAERAH SITU NAJIS BAT SIH HIDUP LU MANA HIDUP NUMPANG DIEMPERAN RUKO MAKAN MINTA MINTA KALO GA KOREK KOREK TONG SAMPAH KALO GADAPET MAKAN DAUN UDAH KEK KAMBING AJA TOLOL UDAH BULU TEBEL DEKIL BAU LAGI KONTOL DIDIK DULU NOH ADEKLU BIAR GA JADI LACUR KEK MAK LU MANA MASIH KECIL SUKA BAT NYEPONGIN KONTOL TETANGGA CUMA PEN DAPET DUIT JAJAN KARENA HIDUP KELUARGALU TERLALU MISKIN KASIAN BAT SIH MENDING LU JADI ANAK BERGUNA DIKIT TOLOL MANA JADI BEBAN HOBI BAT SIH MABOK MABOKAN MANA MABOK KOMIK UDAH HASIL NYOLONG DARI RUKO ORANG DIH KERJAAN NGAMEN DILAMPU MERAH HABIS ITU BEGAYA PASANG TOGEL CUMA GOCENG BERHARAP BAT DAPET HASIL BUAT NYAMBUNG HIDUPLU YANG GA BERGUNA ITU DIH MAKANYA YA JADI ANAK HARAM JAN KEBANYAKAN TINGKAH KONTOL",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "war1": f"**Plugin : **`war1`\
+        \n\n  •  **Syntax :** `{cmd}b`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}c`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}d`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}e`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}f`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}g`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}h`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}i`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}m`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}n`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}o`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}r`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}s`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}t`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}u`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}v`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}w`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}x`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n  •  **Syntax :** `{cmd}z`\
+        \n  •  **Function : **Coba sendiri\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help custom`\
+    "
+    }
+)
