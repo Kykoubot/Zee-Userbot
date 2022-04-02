fc45f932eb67fc2b9717cac021885dc59ed48d3f
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-02 16:19:19 +0800

    y

--- userbot/modules/sql_helper/filter_sql.py
+++ userbot/modules/sql_helper/filter_sql.py
@@ -36,8 +36,7 @@ def get_filter(chat_id, keyword):
 
 def get_filters(chat_id):
     try:
-        return SESSION.query(Filter).filter(
-            Filter.chat_id == str(chat_id)).all()
+        return SESSION.query(Filter).filter(Filter.chat_id == str(chat_id)).all()
     finally:
         SESSION.close()
 
@@ -73,4 +72,3 @@ def remove_all_filters(chat_id):
     if saved_filter:
         saved_filter.delete()
         SESSION.commit()
-
