b3448cd8631703a4ff9b97e65e8498ceac7ad910
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-03 01:13:53 +0800

    y

--- userbot/modules/notes.py
+++ userbot/modules/notes.py
@@ -5,51 +5,52 @@
 #
 """ Userbot module containing commands for keeping notes. """
 
-from asyncio import sleep
-
-from userbot import BOTLOG_CHATID
-from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, edit_delete, man_cmd
 from userbot.events import register
-from userbot.utils import man_cmd
+from asyncio import sleep
 
 
 @man_cmd(pattern="notes$")
 async def notes_active(svd):
+    """ For .notes command, list all of the notes saved in a chat. """
     try:
         from userbot.modules.sql_helper.notes_sql import get_notes
     except AttributeError:
-        return await svd.edit("**Running on Non-SQL mode!**")
-    message = "**Tidak ada catatan yang disimpan dalam obrolan ini**"
+        return await edit_or_reply(svd, "`Running on Non-SQL mode!`")
+    message = "`There are no saved notes in this chat`"
     notes = get_notes(svd.chat_id)
     for note in notes:
-        if message == "**Tidak ada catatan yang disimpan dalam obrolan ini**":
-            message = "**Catatan yang disimpan dalam obrolan ini:**\n"
-        message += "`#{}`\n".format(note.keyword)
-    await svd.edit(message)
+        if message == "`There are no saved notes in this chat`":
+            message = "Notes saved in this chat:\n"
+            message += "`#{}`\n".format(note.keyword)
+        else:
+            message += "`#{}`\n".format(note.keyword)
+    await edit_or_reply(svd, message)
 
 
-@man_cmd(pattern="clear (\w*)")
+@man_cmd(pattern="clear (\\w*)")
 async def remove_notes(clr):
-    """For .clear command, clear note with the given name."""
+    """ For .clear command, clear note with the given name."""
     try:
         from userbot.modules.sql_helper.notes_sql import rm_note
     except AttributeError:
-        return await clr.edit("**Running on Non-SQL mode!**")
+        return await edit_or_reply(clr, "`Running on Non-SQL mode!`")
     notename = clr.pattern_match.group(1)
     if rm_note(clr.chat_id, notename) is False:
-        return await clr.edit(
-            "**Tidak dapat menemukan catatan:** `{}`".format(notename)
-        )
-    return await clr.edit("**Berhasil Menghapus Catatan:** `{}`".format(notename))
+        return await clr.edit("`Couldn't find note:` **{}**".format(notename))
+    else:
+        return await edit_or_reply(clr,
+                                   "`Successfully deleted note:` **{}**".format(notename))
 
 
-@man_cmd(pattern="save (\w*)")
+@man_cmd(pattern="save (\\w*)")
 async def add_note(fltr):
+    """ For .save command, saves notes in a chat. """
     try:
         from userbot.modules.sql_helper.notes_sql import add_note
     except AttributeError:
-        return await fltr.edit("**Running on Non-SQL mode!**")
+        return await edit_or_reply(fltr, "`Running on Non-SQL mode!`")
     keyword = fltr.pattern_match.group(1)
     string = fltr.text.partition(keyword)[2]
     msg = await fltr.get_reply_message()
@@ -57,32 +58,36 @@ async def add_note(fltr):
     if msg and msg.media and not string:
         if BOTLOG_CHATID:
             await fltr.client.send_message(
-                BOTLOG_CHATID,
-                f"#NOTE\nCHAT ID: {fltr.chat_id}\nKEYWORD: {keyword}"
-                "\n\nPesan berikut disimpan sebagai data balasan catatan untuk obrolan, mohon JANGAN dihapus !!",
+                BOTLOG_CHATID, f"#NOTE\nCHAT ID: {fltr.chat_id}\nKEYWORD: {keyword}"
+                "\n\nThe following message is saved as the note's reply data for the chat, please do NOT delete it !!"
             )
-            msg_o = await fltr.client.forward_messages(
-                entity=BOTLOG_CHATID, messages=msg, from_peer=fltr.chat_id, silent=True
-            )
+            msg_o = await fltr.client.forward_messages(entity=BOTLOG_CHATID,
+                                                       messages=msg,
+                                                       from_peer=fltr.chat_id,
+                                                       silent=True)
             msg_id = msg_o.id
         else:
-            return await fltr.edit(
-                "**Menyimpan media sebagai data untuk catatan memerlukan BOTLOG_CHATID untuk disetel.**"
+            return await x.edit(
+                "`Saving media as data for the note requires the BOTLOG_CHATID to be set.`"
             )
     elif fltr.reply_to_msg_id and not string:
         rep_msg = await fltr.get_reply_message()
         string = rep_msg.text
-    success = "**Catatan {} disimpan. Gunakan** `#{}` **untuk mengambilnya**"
+    success = "`Note {} successfully. Use` #{} `to get it`"
     if add_note(str(fltr.chat_id), keyword, string, msg_id) is False:
-        return await fltr.edit(success.format("Berhasil", keyword))
-    return await fltr.edit(success.format("Berhasil", keyword))
+        return await edit_or_reply(fltr, success.format('updated', keyword))
+    else:
+        return await edit_or_reply(fltr, success.format('added', keyword))
 
 
-@register(pattern=r"#\w*", disable_edited=True, disable_errors=True, ignore_unsafe=True)
+@register(pattern=r"#\w*",
+          disable_edited=True,
+          disable_errors=True,
+          ignore_unsafe=True)
 async def incom_note(getnt):
-    """Notes logic."""
+    """ Notes logic. """
     try:
-        if not (await getnt.get_sender()).getnt.client:
+        if not (await getnt.get_sender()).bot:
             try:
                 from userbot.modules.sql_helper.notes_sql import get_note
             except AttributeError:
@@ -92,21 +97,18 @@ async def incom_note(getnt):
             message_id_to_reply = getnt.message.reply_to_msg_id
             if not message_id_to_reply:
                 message_id_to_reply = None
-            if note:
-                if note.f_mesg_id:
-                    msg_o = await getnt.client.get_messages(
-                        entity=BOTLOG_CHATID, ids=int(note.f_mesg_id)
-                    )
-                    await getnt.client.send_message(
-                        getnt.chat_id,
-                        msg_o.mesage,
-                        reply_to=message_id_to_reply,
-                        file=msg_o.media,
-                    )
-                elif note.reply:
-                    await getnt.client.send_message(
-                        getnt.chat_id, note.reply, reply_to=message_id_to_reply
-                    )
+            if note and note.f_mesg_id:
+                msg_o = await getnt.client.get_messages(entity=BOTLOG_CHATID,
+                                                        ids=int(
+                                                            note.f_mesg_id))
+                await getnt.client.send_message(getnt.chat_id,
+                                                msg_o.mesage,
+                                                reply_to=message_id_to_reply,
+                                                file=msg_o.media)
+            elif note and note.reply:
+                await getnt.client.send_message(getnt.chat_id,
+                                                note.reply,
+                                                reply_to=message_id_to_reply)
     except AttributeError:
         pass
 
@@ -117,8 +119,8 @@ async def kick_marie_notes(kick):
         Marie(or her clones) notes from a chat. """
     bot_type = kick.pattern_match.group(1).lower()
     if bot_type not in ["marie", "rose"]:
-        return await kick.edit("`That bot is not yet supported!`")
-    await kick.edit("```Will be kicking away all Notes!```")
+        return await edit_delete(kick, "`That bot is not yet supported!`")
+    await edit_or_reply(kick, "```Will be kicking away all Notes!```")
     await sleep(3)
     resp = await kick.get_reply_message()
     filters = resp.text.split("-")[1:]
@@ -126,14 +128,14 @@ async def kick_marie_notes(kick):
         if bot_type == "marie":
             await kick.reply("/clear %s" % (i.strip()))
         if bot_type == "rose":
-            i = i.replace("`", "")
+            i = i.replace('`', '')
             await kick.reply("/clear %s" % (i.strip()))
         await sleep(0.3)
-    await kick.respond("```Successfully purged bots notes yaay!```\n Gimme cookies!")
-    if BOTLOG_CHATID:
+    await kick.respond(
+        "```Successfully purged bots notes yaay!```\n Gimme cookies!")
+    if BOTLOG:
         await kick.client.send_message(
-            BOTLOG_CHATID, "I cleaned all Notes at " + str(kick.chat_id)
-        )
+            BOTLOG_CHATID, "I cleaned all Notes at " + str(kick.chat_id))
 
 
 CMD_HELP.update(
